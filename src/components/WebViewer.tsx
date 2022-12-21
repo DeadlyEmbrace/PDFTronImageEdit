e46299f612805c7abdb8432daab576c7e99108ed
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2022-12-21 09:41:38 -0700

    Tweaks
    - Changed WebViewer css class name to not collide with other webviewers
    - Changed resource path to match other webviewers

--- src/components/WebViewer.tsx
+++ src/components/WebViewer.tsx
@@ -9,7 +9,7 @@ const PDFTronWebViewer = ({getInstance, uri} : any) : any => {
 
     useEffect(() => {
         WebViewer({
-            path: '/webviewer', // point to where the files you copied are served from
+            path: '../resources/public/', // point to where the files you copied are served from
             fullAPI: true,
           }, viewer.current).then((instance) => {
             // Call APIs here
@@ -24,7 +24,7 @@ const PDFTronWebViewer = ({getInstance, uri} : any) : any => {
       }
     }, [fileUri, instance])
 
-    return (<div className='pdfDiv verticalEditor' id='pdftron-webviewer' ref={viewer}></div>);
+    return (<div className='pdfEditDiv verticalEditor' id='pdftron-webviewer' ref={viewer}></div>);
 };
 
 export default PDFTronWebViewer;
